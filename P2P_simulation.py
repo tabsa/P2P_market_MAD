58247bddba09f4a636671eb216aae0238e84cb69
tsousa <tas.tiago.sousa@gmail.com>
2021-06-04 07:24:27 +0200

    Correcting typos in the code and notebook

--- P2P_simulation.py
+++ P2P_simulation.py
@@ -37,7 +37,7 @@ outcome_agent = [] # List of outcome DF per RL agent
 df_col_name = ['mean_rd', 'final_step', 'energy_target', 'final_state',
                'avg_Q_val', 'std_Q_val', 'mean_regret', 'avg_reg_val', 'std_reg_val']
 policy_agent = [] # List of policy solutions (array) per RL agent
-
+policy_distribution = [] # List of estimator Q_val(arm_j) per RL agent
 #######################################################################################################################
 
 #%% Main Script
@@ -54,7 +54,6 @@ if __name__ == '__main__':
     policy_name = data['agents']['id'] # Upload the same Policies from the training loop
     optimal_policy = data['policy_dist'] # Optimal policy per agent, it is on a List
     policy_sol_epi = np.zeros((6, no_steps, no_episodes))  # Array to store policy solutions per episode
-    policy_distribution = []
     policy_estimate_dist = np.zeros((no_episodes, no_offers, 3)) # 3 values stored per epi and offer, that is why we have a 3rd dimension
     for ag in range(no_RL_agents):
         agent = trading_agent(env, target_bounds, policy_name[ag], e_greedy=1) # Call the trading_agent class
@@ -77,27 +76,33 @@ if __name__ == '__main__':
                 agent.reset()
             else: # Stop the loop
                 break
+        # end episodes for agent ag
+        # Store the outcome parameters:
+        outcome_agent.append(pd.DataFrame(agent.outcome, columns=df_col_name))
+        policy_agent.append(policy_sol_epi)
+        policy_distribution.append(policy_estimate_dist)
+        # Reset the array for next agent in agent_list:
+        policy_sol_epi = np.zeros((6, no_steps, no_episodes))
+        policy_estimate_dist = np.zeros((no_episodes, no_offers, 3))
         print('\n')
     print(f'Validation phase is done')
 
     ## Save simulation results
     # Build a dictionary
-    estimator = {} # Estimator computed from the Training_loop, that was used for this validation
-    validation = {} # Results from the validation phase
-    # Estimator info
-    estimator['episodes'] = training_epi
-    estimator['Arm_Q_j'] = [optimal_policy[i][training_epi, :, 0].mean(axis=0) for i in range(3)]
-    estimator['N_arm'] = [optimal_policy[i][training_epi, :, 1].mean(axis=0) for i in range(3)]
-    estimator['Thom_var'] = [optimal_policy[i][training_epi, :, 2].mean(axis=0) for i in range(3)]
+    validation = {} # Results from the validation phase, including the Estimator computed from the Training loop
     # Validation info
-    validation['target'] = target_sample # Energy_target used for the validation
+    validation['episodes'] = training_epi
+    validation['Arm_Q_j'] = [optimal_policy[i][training_epi, :, 0].mean(axis=0) for i in range(3)]
+    validation['N_arm'] = [optimal_policy[i][training_epi, :, 1].mean(axis=0) for i in range(3)]
+    validation['Thom_var'] = [optimal_policy[i][training_epi, :, 2].mean(axis=0) for i in range(3)]
     validation['episodes'] = no_episodes
     validation['steps'] = no_steps
-    validation['policy_sol'] = policy_agent # Optimal policy per agent and episode
-    validation['policy_dist'] = policy_distribution # Distribution of Q_val per arm_j (partner)
-    # Assign the new results to the data dictionary
-    data['training_estimator'] = estimator
-    data['validation'] = validation
+    data['validation'] = validation # Assign the new 'validation' field to the data.dictionary
+    # Update previous fields of the data dictionary
+    data['simulation']['target'] = target_sample # Energy_target used for the validation
+    data['outcome'] = outcome_agent
+    data['policy_sol'] = policy_agent # Optimal policy per agent and episode
+    data['policy_dist'] = policy_distribution  # Distribution of Q_val per arm_j (partner)
     file = open(out_filename, 'wb')
     pkl.dump(data, file)
     file.close()
